4d07555cc2d5dfd58bf09ee9ba6f3bb15d2fbed7
xuhuan <xuhuan@rongcloud.cn>
2020-11-18 17:35:15 +0800

    add callKit project

---
+++ quickdemo-callKit/quickdemo-callKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  quickdemo-callKit
+//
+//  Created by huan xu on 2020/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+//iOS13以下请添加此属性, 否则无法显示ViewController
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
