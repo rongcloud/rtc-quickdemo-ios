d9b77f6786fc933a5c79a9a9f59b7f688396c468
wangyanxu <wangyanxu@rongcloud.cn>
2021-04-19 16:33:55 +0800

    feat：CallKit

--- CallKit/RCRTCCallKitViewController.m
+++ CallKit/RCRTCCallKitViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "RCRTCCallKitViewController.h"
+#import <RongCallKit/RongCallKit.h>
 
 @interface RCRTCCallKitViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *useridTextField;
+@property (weak, nonatomic) IBOutlet UIButton *callMediaAudio;
+@property (weak, nonatomic) IBOutlet UIButton *callMediaVideo;
 
 @end
 
@@ -16,9 +20,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"RCRTCCallKitViewController viewDidLoad");
+ 
+    //必须初始化, 否则无法收到来电
+    [RCCall sharedRCCall];
 }
 
+/**
+ * 音频通话
+ */
+- (IBAction)callMediaAudio:(UIButton *)sender {
+    if (!self.useridTextField.text ||self.useridTextField.text.length == 0) {
+        return;
+    }
+    [self.useridTextField resignFirstResponder];
+    
+    [[RCCall sharedRCCall] startSingleCall:self.useridTextField.text mediaType:RCCallMediaAudio];
+}
+
+
+/**
+ * 视频通话
+ */
+- (IBAction)callMediaVideo:(UIButton *)sender {
+    
+    if (!self.useridTextField.text ||self.useridTextField.text.length == 0) {
+        return;
+    }
+    [self.useridTextField resignFirstResponder];
+    
+    
+    [[RCCall sharedRCCall] startSingleCall:self.useridTextField.text mediaType:RCCallMediaVideo];
+}
+
+- (IBAction)keyboardHide:(UITextField *)sender {
+    [self.useridTextField resignFirstResponder];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.useridTextField resignFirstResponder];
+}
 /*
 #pragma mark - Navigation
 
