e8c3ef034cc6ddd8291a8a29b181f92795a3c87c
wangyanxu <wangyanxu@rongcloud.cn>
2021-04-29 10:31:54 +0800

    feat: 添加远端发布观看屏幕共享，实现共享的基本流程与 UI

--- ScreenSharing/ScreenShareViewController.m
+++ ScreenSharing/ScreenShareViewController.m
@@ -11,6 +11,8 @@
 #import "UIAlertController+RCRTC.h"
 #import <RongRTCLib/RongRTCLib.h>
 #import "RPSystemBroadcastPickerView+SearchButton.h"
+#import "LiveStreamVideo.h"
+#import "LiveVideoLayoutTool.h"
 
 #define ScreenWidth  [UIScreen mainScreen].bounds.size.width
 #define ScreenHeight [UIScreen mainScreen].bounds.size.height
@@ -23,9 +25,9 @@
 @property (nonatomic, strong) RPSystemBroadcastPickerView *systemBroadcastPickerView;
 @property(nonatomic , strong)RongRTCServerSocket *serverSocket;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
-@property(nonatomic, strong) RCRTCLocalVideoView *localView;
+@property(nonatomic, strong) LiveStreamVideo *localView;
 @property(nonatomic, strong) RCRTCRemoteVideoView *remoteView;
-@property(nonatomic, strong) RCRTCLocalVideoView *localShareView;
+@property(nonatomic, strong) LiveStreamVideo *localShareView;
 @property(nonatomic, strong) RCRTCRemoteVideoView *remoteShareView;
 @property (nonatomic, strong) RCRTCVideoOutputStream *shareVideoOutputStream;
 
@@ -34,7 +36,8 @@
 
 @property (nonatomic, strong) UIButton  *screenShareButton;
 
-@property (nonatomic)NSMutableArray *streamVideos;
+@property (nonatomic)NSMutableArray <LiveStreamVideo *>*streamVideos;
+@property (nonatomic, strong)LiveVideoLayoutTool *layoutTool;
 
 @end
 
@@ -60,9 +63,9 @@
  * 添加本地采集预览界面
  */
 - (void)setupLocalVideoView {
-    self.localView = [[RCRTCLocalVideoView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth/2, ScreenHeight/2)];
-    self.localView.fillMode = RCRTCVideoFillModeAspectFill;
-    [self.containerView addSubview:self.localView];
+    [self.streamVideos addObject:self.localView];
+    [self updateLayoutWithAnimation:YES];
+    
 
 }
 
@@ -96,34 +99,29 @@
     if (button.selected) {
         [[self.systemBroadcastPickerView findButton] sendActionsForControlEvents:UIControlEventTouchUpInside];
 
-    }else{
-        [[self.systemBroadcastPickerView findButton] sendActionsForControlEvents:UIControlEventTouchUpInside];
+        [self.serverSocket createServerSocket];
+        RCRTCLocalVideoView *localShareVideoView = (RCRTCLocalVideoView *)self.localShareView
+        .canvesView;
+
+
+
+
+        localShareVideoView.fillMode = RCRTCVideoFillModeAspectFit;
+        localShareVideoView.frameAnimated = NO;
+
+        NSString *tag = @"RongRTCScreenShare";
+        self.shareVideoOutputStream = [[RCRTCVideoOutputStream alloc] initVideoOutputStreamWithTag:tag];
+    //    self.shareVideoOutputStream.videoSource
+        RCRTCVideoStreamConfig *videoConfig = self.shareVideoOutputStream.videoConfig;
+        videoConfig.videoSizePreset = RCRTCVideoSizePreset320x240;
+        [self.shareVideoOutputStream setVideoConfig:videoConfig];
+        [self.shareVideoOutputStream setVideoView:localShareVideoView];
 
         
-    }
-    
-    [self.serverSocket createServerSocket];
-
-    
-    self.localShareView = [[RCRTCLocalVideoView alloc] initWithFrame:CGRectMake(ScreenWidth/2, 0,ScreenWidth/2, ScreenHeight/2)];
-    [self.containerView addSubview:self.localShareView];
-    
-    
-    self.localShareView.fillMode = RCRTCVideoFillModeAspectFit;
-    self.localShareView.frameAnimated = NO;
-    
-    NSString *tag = @"RongRTCScreenVideo";
-    self.shareVideoOutputStream = [[RCRTCVideoOutputStream alloc] initVideoOutputStreamWithTag:tag];
-//    self.shareVideoOutputStream.videoSource
-    RCRTCVideoStreamConfig *videoConfig = self.shareVideoOutputStream.videoConfig;
-    videoConfig.videoSizePreset = RCRTCVideoSizePreset320x240;
-    [self.shareVideoOutputStream setVideoConfig:videoConfig];
-    [self.shareVideoOutputStream setVideoView:self.localShareView];
-
-    
-    
-    [self.room.localUser publishLiveStream:self.shareVideoOutputStream completion:^(BOOL isSuccess, RCRTCCode code, RCRTCLiveInfo * _Nullable liveInfo) {
-            if (code == RCRTCCodeSuccess) {
+        
+        [self.room.localUser publishStream:self.shareVideoOutputStream
+                                completion:^(BOOL isSuccess, RCRTCCode desc) {
+            if (desc == RCRTCCodeSuccess) {
                 [self.streamVideos addObject:self.localShareView];
                 [self updateLayoutWithAnimation:YES];
                 
@@ -131,19 +129,41 @@
             else {
                 
             }
-    }];
-}
+        }];
+    }else{
+        [[self.systemBroadcastPickerView findButton] sendActionsForControlEvents:UIControlEventTouchUpInside];
 
--(void)updateLayoutWithAnimation:(BOOL)isAnimation{
-    switch (self.streamVideos.count) {
-        case 1:
-            break;
-            
-        default:
-            break;
+        [self.room.localUser unpublishStream:self.shareVideoOutputStream
+                                  completion:^(BOOL isSuccess, RCRTCCode desc) {
+            if (isSuccess) {
+                [self.streamVideos removeObject:self.localShareView];
+                self.shareVideoOutputStream = nil;
+                [self updateLayoutWithAnimation:YES];
+            }
+        }];
     }
     
+   
 }
+/**
+ * 布局视图动画
+ */
+- (void)updateLayoutWithAnimation:(BOOL)animation{
+    if (animation) {
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.layoutTool layoutVideos:self.streamVideos inContainer:self.containerView];
+        }];
+    }else{
+        [self.layoutTool layoutVideos:self.streamVideos inContainer:self.containerView];
+    }
+}
+- (void)cleanRemoteContainer{
+    [self.streamVideos removeAllObjects];
+    for (UIView *subview in self.containerView.subviews) {
+        [subview removeFromSuperview];
+    }
+}
+
 #pragma mark - RTC
 
 /**
@@ -185,8 +205,12 @@
     self.room = room;
     room.delegate = self;
 
+    RCRTCLocalVideoView *view = (RCRTCLocalVideoView *)self.localView.canvesView;
+
+    view.fillMode = RCRTCVideoFillModeAspectFill;
+
     // 开始本地视频采集
-    [[self.engine defaultVideoStream] setVideoView:self.localView];
+    [[self.engine defaultVideoStream] setVideoView:view];
     [[self.engine defaultVideoStream] startCapture];
 
     // 发布本地视频流
@@ -209,19 +233,19 @@
 /**
  * 订阅房间中远端用户音视频流资源
  */
-- (void)subscribeRemoteResource:(NSArray<RCRTCInputStream *> *)streams {
-     
-    [self.room.localUser subscribeStream:streams tinyStreams:nil completion:^(BOOL isSuccess, RCRTCCode desc) {
-        
-    }];
-    // 创建并设置远端视频预览视图
-    for (RCRTCInputStream *stream in streams) {
-        if (stream.mediaType == RTCMediaTypeVideo) {
-            [(RCRTCVideoInputStream *) stream setVideoView:self.remoteView];
-            [self.remoteView setHidden:NO];
-        }
-    }
-}
+//- (void)subscribeRemoteResource:(NSArray<RCRTCInputStream *> *)streams {
+//
+//    [self.room.localUser subscribeStream:streams tinyStreams:nil completion:^(BOOL isSuccess, RCRTCCode desc) {
+//
+//    }];
+//    // 创建并设置远端视频预览视图
+//    for (RCRTCInputStream *stream in streams) {
+//        if (stream.mediaType == RTCMediaTypeVideo) {
+//            [(RCRTCVideoInputStream *) stream setVideoView:self.remoteView];
+//            [self.remoteView setHidden:NO];
+//        }
+//    }
+//}
 
 
 -(void)viewWillDisappear:(BOOL)animated{
@@ -246,30 +270,186 @@
     
 }
 
+/**
+ * 取消订阅远端流
+ */
+-(void)unsubscribeRemoteResource:(NSArray<RCRTCInputStream *> *)streams orStreamId:(NSString *)streamId {
+    
+    for (RCRTCInputStream *stream in streams) {
+        if (stream.mediaType == RTCMediaTypeVideo) {
+            streamId = stream.streamId;
+            [self fetchStreamVideoOffLineWithStreamId:streamId];
+        }
+    }
+    
+}
+
+
+/**
+ * 订阅远端流
+ */
+- (void)subscribeRemoteResource:(NSArray<RCRTCInputStream *> *)streams{
+    [self subscribeRemoteResource:streams isTiny:NO];
+}
+
+
+- (void)subscribeRemoteResource:(NSArray<RCRTCInputStream *> *)streams isTiny:(BOOL)isTiny{
+    
+    // 订阅房间中远端用户音视频流资源
+    NSArray *tinyStream = isTiny ? streams : @[];
+    NSArray *ordinaryStream = isTiny ? @[] : streams;
+    [self.room.localUser subscribeStream:ordinaryStream
+                             tinyStreams:tinyStream
+                              completion:^(BOOL isSuccess, RCRTCCode desc) {
+        if (desc != RCRTCCodeSuccess) {
+            NSString *errorStr = [NSString stringWithFormat:@"订阅远端流失败:%ld",(long)desc];
+            [UIAlertController alertWithString:errorStr inCurrentViewController:nil];
+            return;
+        }
+        
+        
+        // 创建并设置远端视频预览视图
+        NSInteger i = 0;
+        for (RCRTCInputStream *stream in streams) {
+            if (stream.mediaType == RTCMediaTypeVideo) {
+                [self setupRemoteViewWithStream:stream];
+                i++;
+            }
+        }
+        if (i > 0) {
+            [self updateLayoutWithAnimation:YES];
+        }
+    }];
+}
+
+/**
+ * 创建并设置远端视频预览视图
+ */
+-(LiveStreamVideo *)setupRemoteViewWithStream:(RCRTCInputStream *)stream{
+    
+    LiveStreamVideo *sVideo = [self creatStreamVideoWithStreamId:stream.streamId];
+    RCRTCRemoteVideoView *remoteView = (RCRTCRemoteVideoView *)sVideo.canvesView;
+    
+    //如果为屏幕共享则适配显示
+    if([stream.tag isEqualToString:@"RongRTCScreenShare"]){
+        
+        remoteView.fillMode = RCRTCVideoFillModeAspectFit;
+        
+    }
+    //设置视频流的渲染视图
+    [(RCRTCVideoInputStream *)stream setVideoView:remoteView];
+    return sVideo;
+}
+
+
+/**
+ *判断是否已有预览视图
+ */
+- (LiveStreamVideo *)creatStreamVideoWithStreamId:(NSString *)streamId{
+    LiveStreamVideo *sVideo = [self fetchStreamVideoWithStreamId:streamId];
+    if (!sVideo) {
+        sVideo = [[LiveStreamVideo alloc] initWithStreamId:streamId];
+        [self.streamVideos insertObject:sVideo atIndex:0];
+    }
+    return sVideo;
+}
+
+/**
+ * 根据 streamId 确认唯一的音视频流
+ */
+- (LiveStreamVideo *)fetchStreamVideoWithStreamId:(NSString *)streamId{
+    for (LiveStreamVideo *sVideo in self.streamVideos) {
+        if ([streamId isEqualToString:sVideo.streamId]) {
+            return sVideo;
+        }
+    }
+    return nil;
+}
+
+/**
+ * 远端掉线/离开回掉调用，删除远端用户的所有音视频流
+ */
+- (void)fetchStreamVideoOffLineWithStreamId:(NSString *)streamId{
+    NSArray *arr = [NSArray arrayWithArray:self.streamVideos];
+    for (LiveStreamVideo *sVideo in arr) {
+        if ([streamId isEqualToString:sVideo.streamId]) {
+            if (sVideo) {
+                [sVideo.canvesView removeFromSuperview];
+                [self.streamVideos removeObject:sVideo];
+                
+            }
+        }
+    }
+    [self updateLayoutWithAnimation:YES];
+}
+
 #pragma mark - RCRTCRoomEventDelegate
 
 /**
  * 远端用户发布资源通知
  */
-- (void)didPublishStreams:(NSArray<RCRTCInputStream *> *)streams {
+-(void)didPublishStreams:(NSArray<RCRTCInputStream *> *)streams{
+    
     [self subscribeRemoteResource:streams];
 }
 
 /**
- * 远端用户取消发布资源通知
+ * 远端用户取消发布资源
  */
-- (void)didUnpublishStreams:(NSArray<RCRTCInputStream *> *)streams {
-    [self.remoteView setHidden:YES];
+- (void)didUnpublishStreams:(NSArray<RCRTCInputStream *> *)streams{
+    
+    [self unsubscribeRemoteResource:streams orStreamId:nil];
+}
+/**
+ * 直播合流发布
+ */
+- (void)didPublishLiveStreams:(NSArray<RCRTCInputStream*> *)streams{
+    
+    [self subscribeRemoteResource:streams];
 }
 
 /**
- * 远端用户离开通知
+ * 直播合流取消发布
  */
-- (void)didLeaveUser:(RCRTCRemoteUser *)user {
-    [self.remoteView setHidden:YES];
+- (void)didUnpublishLiveStreams:(NSArray<RCRTCInputStream*> *)streams{
+    
+    [self unsubscribeRemoteResource:streams orStreamId:nil];
 }
 
+
+/**
+ * 新用户加入
+ */
+- (void)didJoinUser:(RCRTCRemoteUser *)user{
+    
+}
+
+
+/**
+ * 离开
+ */
+- (void)didLeaveUser:(RCRTCRemoteUser *)user{
+    
+    [self unsubscribeRemoteResource:user.remoteStreams orStreamId:nil];
+}
+
+
+/**
+ * 远端掉线
+ */
+- (void)didOfflineUser:(RCRTCRemoteUser*)user{
+    [self unsubscribeRemoteResource:user.remoteStreams orStreamId:nil];
+}
+
+
 #pragma mark - lazy load
+
+- (LiveStreamVideo *)localShareView{
+    if (!_localShareView) {
+        _localShareView = [LiveStreamVideo LocalStreamVideo];
+    }
+    return _localShareView;
+}
 
 - (RCRTCEngine *)engine {
     if (!_engine) {
@@ -277,6 +457,25 @@
         [_engine enableSpeaker:YES];
     }
     return _engine;
+}
+
+- (NSMutableArray<LiveStreamVideo *> *)streamVideos{
+    if (!_streamVideos) {
+        _streamVideos = [NSMutableArray array];
+    }
+    return _streamVideos;
+}
+- (LiveStreamVideo *)localView{
+    if (!_localView) {
+        _localView = [LiveStreamVideo LocalStreamVideo];
+    }
+    return _localView;
+}
+- (LiveVideoLayoutTool *)layoutTool{
+    if (!_layoutTool) {
+        _layoutTool = [LiveVideoLayoutTool new];
+    }
+    return _layoutTool;
 }
 
 -(RongRTCServerSocket *)serverSocket{
