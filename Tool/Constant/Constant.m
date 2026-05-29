07ec6afe13857f6d2620ba84cf8079fed6e653d8
wangyanxu <wangyanxu@rongcloud.cn>
2026-05-29 09:51:14 +0800

    feat： 修改信息输入

--- Tool/Constant/Constant.m
+++ Tool/Constant/Constant.m
@@ -13,8 +13,8 @@
  可选：在代码中填写 App Key 与 Token。
  如果保持为空，运行时可在登录页手动输入。
  */
-NSString *const AppKey = @"pvxdm17jpw0pr";
+NSString *const AppKey = ;
 
-NSString *const Token = @"J0w9dUpIsZE7t2iawWHcKxLPLVpGpkP2NbTsR1U3XU0=@44lh.cn.rongnav.com;44lh.cn.rongcfg.com";
+NSString *const Token = ;
 
 @end
